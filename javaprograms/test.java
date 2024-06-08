a39dab47ed6209a1dd4da72f41818044bb30eee0
Kedar Dhage <kedardhage0@gmail.com>
2024-06-08 14:38:43 +0530

    adding comments

--- javaprograms/test.java
+++ javaprograms/test.java
@@ -3,3 +3,10 @@ public class test {
         System.out.println("Hello, Kedar!");
     }
 }
+/*
+ *  this multiple line comment 
+ * java is high level langauge
+ * java is class independent langauge
+ * java is an object orinted  
+ * 
+ */
